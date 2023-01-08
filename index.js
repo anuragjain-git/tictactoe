2cfb713df214718f7cb61d00b8755aa97335b77f
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2023-01-08 16:43:41 +0530

    uploaded all files
    html, css, js

---
+++ index.js
@@ -0,0 +1,83 @@
+const board = document.querySelector("#board");
+let player = "X";
+$("p").hide();
+board.addEventListener("click", function(event) {
+    if(event.target.textContent != "") {
+        return;
+    }
+
+    // Assign player
+
+    event.target.textContent = player;
+
+    // Check for win
+
+    if(win()) {
+        // alert(player+" Won.");
+        $("#p").show();
+        $("#p").text(player+' Won!');
+        player = "X";
+    }
+    else if (draw()) {
+        $("#p").show();
+        $("#p").text('Draw!');
+    }
+
+    // Change Player
+
+    if(player == "X") {
+        player = "O";
+    }
+    else {
+        player = "X";
+    }
+
+});
+
+// code for win or draw
+
+function win () {
+    for (let i = 0; i<3; i++ ){
+        if((board.rows[i].cells[0].textContent == player) && (board.rows[i].cells[1].textContent == player) && (board.rows[i].cells[2].textContent == player) ) 
+            return true;
+    }
+
+    for (var i = 0; i<3; i++ ){
+        if((board.rows[0].cells[i].textContent == player) && (board.rows[1].cells[i].textContent == player) && (board.rows[2].cells[i].textContent == player) ) 
+            return true;
+    }
+
+    if((board.rows[0].cells[0].textContent == player) && (board.rows[1].cells[1].textContent == player) && (board.rows[2].cells[2].textContent == player) ) 
+       return true;
+
+    if((board.rows[0].cells[2].textContent == player) && (board.rows[1].cells[1].textContent == player) && (board.rows[2].cells[0].textContent == player) ) 
+       return true;
+}
+
+function draw() {
+    if((board.rows[0].cells[0].textContent != "") && (board.rows[0].cells[1].textContent != "") && (board.rows[0].cells[2].textContent != "") &&
+    (board.rows[1].cells[0].textContent != "") && (board.rows[1].cells[1].textContent != "") && (board.rows[1].cells[2].textContent != "") &&
+    (board.rows[0].cells[2].textContent != "") && (board.rows[2].cells[1].textContent != "") && (board.rows[2].cells[2].textContent != "")) 
+         return true;
+}
+
+
+// function reset() {
+//     for (var i = 0; i<3; i++ ){
+//         board.rows[0].cells[i].textContent = "";
+//         board.rows[1].cells[i].textContent = "";
+//         board.rows[2].cells[i].textContent = "";
+//     }
+//     $("#p").hide();
+// }
+
+//reset
+
+$("#reset").click(function() {
+    for (var i = 0; i<3; i++ ){
+        board.rows[0].cells[i].textContent = "";
+        board.rows[1].cells[i].textContent = "";
+        board.rows[2].cells[i].textContent = "";
+    }
+    $("#p").hide();
+});
